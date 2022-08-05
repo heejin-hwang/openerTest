5a4b75591e98de90a755f956a07651f7686a1472
chloe <chloe.jin@kakaoenterprise>
2022-08-05 17:00:17 +0900

    feat: post test4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // console.log('kepTransactionId', window.opener.document.myForm.kepTransactionId);
+  // console.log('userAuthToken', window.opener.document.myForm.userAuthToken);
+  const param = window.opener.document.getElementById('myForm').value || '없다'
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>{param}</div>
         <button onClick={() => window.location.href = 'https://local.dev.kakaoi.io:3000/billing/success.html'}>성공url로 직접</button>
         <button onClick={() => window.location.href = 'https://local.dev.kakaoi.io:3000/billing/fail.html'}>실패url로 직접</button>
         <button onClick={() => window.location.href = 'https://local.dev.kakaoi.io:3000/billing/callback'}>html로 안보내면?</button>
